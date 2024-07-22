7dd780d03a9f519d85613fd037620827fc86fad7
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-22 12:11:44 +0530

    Initial Commit

---
+++ Game Player Fun/Game_Player_FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Player_FunApp.swift
+//  Game Player Fun
+//
+//  Created by surendra on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_Player_FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
