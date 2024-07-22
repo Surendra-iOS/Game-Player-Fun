2f49a65acec8dd6624d2379e9a3d860a0b2339f3
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-22 12:32:04 +0530

    Game Player Fun

---
+++ Game Player Fun/PlayerList.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerList.swift
+//  Game Player Fun
+//
+//  Created by surendra on 22/07/24.
+//
+
+import SwiftUI
+
+struct PlayerList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerList()
+}
---
+++ Game Player Fun/PlayerRow.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerRow.swift
+//  Game Player Fun
+//
+//  Created by surendra on 22/07/24.
+//
+
+import SwiftUI
+
+struct PlayerRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerRow()
+}
